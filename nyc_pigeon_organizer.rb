2a25648cf66d5e67648f47ae10e18e11b5996331
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-03-02 18:09:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,17 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.reduce({}) do |memo, (key, value)|
-    memo[key] = value.sort
-    pp memo
+  data.each do |key, value|
+    value.each do |new_value, names|
+    names.each do |name|
+      if !pigeon_list[name]
+        pigeon_list[name] = {}
+      end
+      if !pigeon_list[name][key]
+        pigeon_list[name][key] = []
+      end
+      pigeon_list[name][key] << new_value.to_s
+      end
+    end
   end
+  pigeon_list
 end
